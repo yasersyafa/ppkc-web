e1ba15293b0ae16f5365f35ce2b7816fac2b5727
Yaser Syafa <syafayaser@gmail.com>
2026-01-10 06:31:14 +0700

    fix: z index whatsapp

--- components/whatsapp-button.tsx
+++ components/whatsapp-button.tsx
@@ -6,7 +6,12 @@ export function WhatsAppButton({ phoneNumber }: { phoneNumber: string }) {
   const whatsappLink = `https://wa.me/${phoneNumber}`;
 
   return (
-    <Link href={whatsappLink} target="_blank" rel="noopener noreferrer">
+    <Link
+      href={whatsappLink}
+      target="_blank"
+      rel="noopener noreferrer"
+      className="z-50"
+    >
       <Button
         size={"icon-lg"}
         className="fixed bottom-5 right-5 rounded-full p-4 shadow-lg flex items-center justify-center"
