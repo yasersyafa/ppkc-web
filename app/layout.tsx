fd09b135e91b4fc183bf1172321517271809904b
Yaser Syafa <syafayaser@gmail.com>
2026-01-10 06:13:21 +0700

    feat: add favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,12 +22,7 @@ export const metadata: Metadata = {
         url: "/icon-dark-32x32.png",
         media: "(prefers-color-scheme: dark)",
       },
-      {
-        url: "/icon.svg",
-        type: "image/svg+xml",
-      },
     ],
-    apple: "/apple-icon.png",
   },
 };
 
